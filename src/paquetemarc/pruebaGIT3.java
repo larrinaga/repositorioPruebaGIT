25e656447dffa90b18a3e8a65b9b04afdaba8cb4
Marc <lekazki90@gmail.com>
2013-05-03 12:08:24 +0200

    primera version del proyecto

---
+++ src/paquetemarc/pruebaGIT3.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquetemarc;
+
+/**
+ *
+ * @author Marc
+ */
+public class pruebaGIT3 {
+    public pruebaGIT3(){
+        System.out.println("hola, estamos en la tercera clase");
+    }
+}
