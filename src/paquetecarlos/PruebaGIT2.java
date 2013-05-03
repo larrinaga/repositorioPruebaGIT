4278c5fcac913e6e79dcbd338f54f567580cfc49
Diurno <Diurno@10-C7>
2013-05-03 11:37:02 +0200

    Segunda versión del proyecto con otra clase

---
+++ src/paquetecarlos/PruebaGIT2.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquetecarlos;
+
+/**
+ *
+ * @author Diurno
+ */
+public class PruebaGIT2 {
+    public PruebaGIT2(){
+        System.out.println("Esta es la segunda clase.");
+    }
+}
