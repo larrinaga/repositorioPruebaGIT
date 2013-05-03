7714d7ea710e13954f50020ad4d3b9ddde651799
Diurno <Diurno@10-C7>
2013-05-03 11:06:14 +0200

    Primera versión del proyecto

---
+++ src/paquetecarlos/PruebaGIT.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquetecarlos;
+
+/**
+ *
+ * @author Diurno
+ */
+public class PruebaGIT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+}
